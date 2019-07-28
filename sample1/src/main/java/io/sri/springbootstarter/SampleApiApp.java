11a81354eb1c5130bd1a08be8966a01caa46f87d
srpatoju <srpatoju@SRPATOJU-PZS1D.cisco.com>
2019-07-28 14:33:37 +0530

    First commit

---
+++ sample1/src/main/java/io/sri/springbootstarter/SampleApiApp.java
@@ -0,0 +1,14 @@
+package io.sri.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApiApp.class, args);
+
+	}
+
+}
---
+++ sample1/src/main/java/io/sri/springbootstarter/topic/TopicService.java
@@ -0,0 +1,44 @@
+package io.sri.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+
+			new Topic("Spring", "Spring Framework", "Spring FrameWork Description"),
+			new Topic("Java", "Java Framework", "Java FrameWork Description"),
+			new Topic("JavaScript", "JavaScript Framework", "JavaScript FrameWork Description")
+			));
+
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	
+	  public void addTopic(Topic topic) { 
+		  topics.add(topic);
+	  
+	  }
+
+	public void updateTopic(String id,Topic topic) {
+		
+		for(int i=0 ; i< topics.size() ; i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+	 
+}
